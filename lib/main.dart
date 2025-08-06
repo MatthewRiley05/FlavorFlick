c0ab790ca5babdd4755728140097c83fcb89cc47
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-08-06 11:03:18 +0800

    fix: add dark theme and theme mode to MaterialApp in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlavorFlick',
       theme: ThemeData(useMaterial3: true),
+      darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
       home: HomeScreen(),
     );
   }
