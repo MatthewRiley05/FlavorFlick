040d228d0ff5fc83006947f34a9775eb021bbea9
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-08-06 15:15:44 +0800

    refactor: convert SettingsScreen to StatelessWidget and streamline theme mode handling in ThemeNotifier

--- lib/modules/settings/src/screens/settings_screen.dart
+++ lib/modules/settings/src/screens/settings_screen.dart
@@ -3,7 +3,7 @@ import 'package:flavor_flick/modules/settings/src/bookmark_settings.dart';
 import 'package:flavor_flick/modules/settings/src/map_settings.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({
     super.key,
     required this.onBookmarkSubmitted,
@@ -13,20 +13,15 @@ class SettingsScreen extends StatefulWidget {
   final ValueChanged<String> onSearchTypeChanged;
 
   @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  @override
   Widget build(BuildContext context) => Scaffold(
     body: SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16),
         child: Column(
           children: [
-            BookmarkSettings(onBookmarkSubmitted: widget.onBookmarkSubmitted),
+            BookmarkSettings(onBookmarkSubmitted: onBookmarkSubmitted),
             AppearanceSettings(),
-            MapSettings(onSearchTypeChanged: widget.onSearchTypeChanged),
+            MapSettings(onSearchTypeChanged: onSearchTypeChanged),
           ],
         ),
       ),
